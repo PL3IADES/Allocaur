0b8318ee546b51118bed5a82bb2f77db4bd65f6e
PL3IADES <milobeckshomework@gmail.com>
2020-07-04 14:39:36 -0600

    master

---
+++ Allocaur.java
@@ -0,0 +1,30 @@
+package Allocaur;
+import java.util.Scanner;
+// TO ALL WHO SEE THIS, READ LINE 26!!
+public class Allocaur {
+    public static void main(String[] args) {
+        String name;
+        Scanner s = new Scanner(System.in); 
+        System.out.println("Welcome to the world of Allocaur.");
+        System.out.println("A new adventure awaits you.");
+        System.out.print("Enter you name: ");
+        name = s.nextLine();
+        System.out.println("so your names " + name + " huh?");
+        System.out.println("Well, i dont really care about that. i might use it in the future,"); 
+        System.out.println("just to get your attention.");
+        System.out.println();
+        System.out.println("Anyway, i probably should get on with why your here.");
+        System.out.println("You just came from the recriutment office of Allocaur Industries.");
+        System.out.println("what is Allocaur Industries you may ask?");
+        System.out.println("Well you shoudlve figured that out when you signed up to be a bounty hunter.");
+        System.out.println("you have joined the only established bounty hunting establishment in the galaxy.");
+        System.out.println("i almost forgot to tell you, my names \"The Narrator\". i only talk to you.");
+        System.out.println("I'm the voice inside your head basically speaking. when you joined Allocaur,");
+        System.out.println("your longterm memory was wiped, and an implant was placed in the back of your skull.");
+        System.out.println("thats how Allocaur recognizes you and thats where i live");
+        System.out.println();
+        System.out.println("wow this is getting pretty hairy, lets freshen up this story a little bit huh? how does that sound?");
+        
+        s.close();
+    }
+}
---
+++ gameCommands.java
@@ -0,0 +1,7 @@
+package Allocaur;
+
+public class gameCommands {
+    public void printStuff(){
+        System.out.println("this is B.S. for now............");
+    }
+}
