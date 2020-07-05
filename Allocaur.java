8c5dfa70fd9c29167eb386b5f2d9cd197f0381f9
PL3IADES <milobeckshomework@gmail.com>
2020-07-04 23:22:26 -0600

    origin

--- Allocaur.java
+++ Allocaur.java
@@ -11,11 +11,17 @@ public class Allocaur {
         name = s.nextLine();
         System.out.println("so your names " + name + " huh?");
         System.out.println("Well, i dont really care about that. i might use it in the future,"); 
-        System.out.println("just to get your attention.");
-        System.out.println();
+        System.out.println("just to get your attention.\n");
+        System.out.println("my name is nicodemus, i am the voice inside your head.");
+        System.out.println("ever since you were a kid i have been here.");
+        System.out.println("listening. thinking. i dont know where i come from. but you have your suspicions of where i might.");
+        System.out.println("but enough of that, you have finally come of age, therfore, i can reveal my only secret.");
+        System.out.println("the legend of Allocaur.\n");
+        System.out.println("you are in a dark room \n");
+        System.out.println("in this game, if text is lined with \n than its instructions from the game itself");
+        System.out.println("along with the game instructions, if a line contains, it is game relations and your own thoughts.");
+        System.out.println("anyway, along with the game.");
         
-        System.out.println("wow this is getting pretty hairy, lets freshen up this story a little bit huh? how does that sound?");
         
-        s.close();
     }
 }
--- gameCommands.java
+++ gameCommands.java
@@ -3,5 +3,6 @@ package Allocaur;
 public class gameCommands {
     public void printStuff(){
         System.out.println("this is B.S. for now...");
+        
     }
 }
