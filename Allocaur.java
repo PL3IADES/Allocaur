552edfa1a460935e8b4c5cf9b59fa4ade8376c47
PL3IADES <milobeckshomework@gmail.com>
2020-07-04 19:45:34 -0600

    origin

--- Allocaur.java
+++ Allocaur.java
@@ -1,6 +1,6 @@
 package Allocaur;
 import java.util.Scanner;
-// TO ALL WHO SEE THIS, READ LINE 26 (W.I.P.)
+
 public class Allocaur {
     public static void main(String[] args) {
         String name;
@@ -13,16 +13,7 @@ public class Allocaur {
         System.out.println("Well, i dont really care about that. i might use it in the future,"); 
         System.out.println("just to get your attention.");
         System.out.println();
-        System.out.println("Anyway, i probably should get on with why your here.");
-        System.out.println("You just came from the recriutment office of Allocaur Industries.");
-        System.out.println("what is Allocaur Industries you may ask?");
-        System.out.println("Well you shoudlve figured that out when you signed up to be a bounty hunter.");
-        System.out.println("you have joined the only established bounty hunting establishment in the galaxy.");
-        System.out.println("i almost forgot to tell you, my names \"The Narrator\". i only talk to you.");
-        System.out.println("I'm the voice inside your head basically speaking. when you joined Allocaur,");
-        System.out.println("your longterm memory was wiped, and an implant was placed in the back of your skull.");
-        System.out.println("thats how Allocaur recognizes you and thats where i live");
-        System.out.println();
+        
         System.out.println("wow this is getting pretty hairy, lets freshen up this story a little bit huh? how does that sound?");
         
         s.close();
